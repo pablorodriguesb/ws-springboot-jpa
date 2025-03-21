8f54f610c2029a37db62d9a0a656eee6c56237e1
Pablo Rodrigues <pablo.rodriguesbat@hotmail.com>
2025-03-21 15:25:56 -0300

    Pedido, Instante, ISO 8601 - Parte 1

---
+++ src/main/java/com/devspringpablo/course/entities/Order.java
@@ -0,0 +1,82 @@
+package com.devspringpablo.course.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_order")
+public class Order implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+    @ManyToOne // muitos para um 
+    @JoinColumn(name = "client_id")
+    private User client;
+
+    public Order() {
+    }
+
+    public Order(Long id, Instant moment, User client) {
+        this.id = id;
+        this.moment = moment;
+        this.client = client;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+
+    public User getClient() {
+        return client;
+    }
+
+    public void setClient(User client) {
+        this.client = client;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 11 * hash + Objects.hashCode(this.id);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Order other = (Order) obj;
+        return Objects.equals(this.id, other.id);
+    }
+
+}
--- src/main/java/com/devspringpablo/course/entities/User.java
+++ src/main/java/com/devspringpablo/course/entities/User.java
@@ -6,7 +6,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tb_user")
@@ -19,6 +22,9 @@ public class User implements Serializable{
     private String email;
     private String phone;
     private String password;
+    
+    @OneToMany(mappedBy = "client") // um para muitos
+    private List<Order> orders = new ArrayList<>();
     
     public User() {
     }
@@ -71,6 +77,10 @@ public class User implements Serializable{
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+    
     @Override
     public int hashCode() {
         int hash = 7;
