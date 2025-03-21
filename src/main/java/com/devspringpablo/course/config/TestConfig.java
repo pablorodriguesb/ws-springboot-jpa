064fac2bee56fa50adde652de2487e0067884a03
Pablo Rodrigues <pablo.rodriguesbat@hotmail.com>
2025-03-21 19:02:05 -0300

    OrderStatus enum

--- src/main/java/com/devspringpablo/course/config/TestConfig.java
+++ src/main/java/com/devspringpablo/course/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.devspringpablo.course.config;
 
 import com.devspringpablo.course.entities.Order;
 import com.devspringpablo.course.entities.User;
+import com.devspringpablo.course.entities.enums.OrderStatus;
 import com.devspringpablo.course.repositories.OrderRepository;
 import com.devspringpablo.course.repositories.UserRepository;
 import java.time.Instant;
@@ -26,9 +27,9 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Pablo Rodrigues", "pablo@gmail.com", "19994932321", "123456");
         User u2 = new User(null, "Weslei Batista", "weslei@gmail.com", "19994234232", "123456");
 
-        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), u2);
-        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"), OrderStatus.PAID, u1);
+        Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
+        Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMENT, u1);
         userRepository.saveAll(Arrays.asList(u1, u2));
         orderRepository.saveAll(Arrays.asList(o1, o2, o3));
     }
--- src/main/java/com/devspringpablo/course/entities/Order.java
+++ src/main/java/com/devspringpablo/course/entities/Order.java
@@ -1,5 +1,6 @@
 package com.devspringpablo.course.entities;
 
+import com.devspringpablo.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -19,9 +20,11 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
+
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
+
+    private Integer orderStatus;
 
     @ManyToOne // muitos para um 
     @JoinColumn(name = "client_id")
@@ -30,9 +33,10 @@ public class Order implements Serializable {
     public Order() {
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -50,6 +54,16 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+            this.orderStatus = orderStatus.getCode();
+        }
     }
 
     public User getClient() {
