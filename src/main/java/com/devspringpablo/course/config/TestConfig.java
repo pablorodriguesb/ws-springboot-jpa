3b4e032e60487c9a2c4cc28129b069458b9c1a52
Pablo Rodrigues <pablo.rodriguesbat@hotmail.com>
2025-03-21 20:36:32 -0300

    Associação muitos-para-muitos com JoinTable

--- src/main/java/com/devspringpablo/course/config/TestConfig.java
+++ src/main/java/com/devspringpablo/course/config/TestConfig.java
@@ -48,6 +48,15 @@ public class TestConfig implements CommandLineRunner {
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+        p1.getCategories().add(cat2);
+        p2.getCategories().add(cat1);
+        p2.getCategories().add(cat3);
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+        
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+        
         User u1 = new User(null, "Pablo Rodrigues", "pablo@gmail.com", "19994932321", "123456");
         User u2 = new User(null, "Weslei Batista", "weslei@gmail.com", "19994234232", "123456");
 
--- src/main/java/com/devspringpablo/course/entities/Category.java
+++ src/main/java/com/devspringpablo/course/entities/Category.java
@@ -1,11 +1,12 @@
 package com.devspringpablo.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Objects;
@@ -21,7 +22,8 @@ public class Category implements Serializable {
     private Long id;
     private String name;
     
-    @Transient
+    @JsonIgnore
+    @ManyToMany(mappedBy = "categories")
     private Set<Product> products = new HashSet<>();
 
     public Category() {
