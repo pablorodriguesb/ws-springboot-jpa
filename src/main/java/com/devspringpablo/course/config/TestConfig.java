a70076ebe58e757bccffa6bbfd8381e9fe44b0fa
Pablo Rodrigues <pablo.rodriguesbat@hotmail.com>
2025-03-22 10:46:33 -0300

    Pagamento, associação um-para-um

--- src/main/java/com/devspringpablo/course/config/TestConfig.java
+++ src/main/java/com/devspringpablo/course/config/TestConfig.java
@@ -3,6 +3,7 @@ package com.devspringpablo.course.config;
 import com.devspringpablo.course.entities.Category;
 import com.devspringpablo.course.entities.Order;
 import com.devspringpablo.course.entities.OrderItem;
+import com.devspringpablo.course.entities.Payment;
 import com.devspringpablo.course.entities.Product;
 import com.devspringpablo.course.entities.User;
 import com.devspringpablo.course.entities.enums.OrderStatus;
@@ -77,6 +78,11 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
         
         orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+        
+        Payment pay1 = new Payment(null,Instant.parse("2019-06-20T21:53:07Z"), o1);
+        o1.setPayment(pay1);
+        
+        orderRepository.save(o1);
     }
 
 }
