587d6aa6de78f812d8d4e2c982d4d00e39000c20
Pablo Rodrigues <pablo.rodriguesbat@hotmail.com>
2025-03-23 13:43:10 -0300

    Tratamento de exceções - Atualizar

--- src/main/java/com/devspringpablo/course/services/UserService.java
+++ src/main/java/com/devspringpablo/course/services/UserService.java
@@ -4,6 +4,7 @@ import com.devspringpablo.course.entities.User;
 import com.devspringpablo.course.repositories.UserRepository;
 import com.devspringpablo.course.services.exceptions.DatabaseException;
 import com.devspringpablo.course.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -46,9 +47,13 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
+        try {
         User entity = repository.getReferenceById(id);
         updateData(entity, obj);
         return repository.save(entity);
+        } catch (EntityNotFoundException e) {
+            throw new ResourceNotFoundException(id);
+        }
     }
 
     private void updateData(User entity, User obj) {
